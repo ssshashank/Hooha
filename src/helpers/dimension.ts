bc3c6fe57ba595d94633fbd6f64bc07623d1ba01
Shekhar S <shekharshashank97@gmail.com>
2024-10-13 14:59:04 +0530

    Dimensions
    Created dimension utilities for widgets.

--- src/helpers/dimension.ts
+++ src/helpers/dimension.ts
@@ -1,11 +1,44 @@
+// imports
 import { Dimensions } from 'react-native';
 
-const {
-    width: deviceWidth,
-    height: deviceHeight,
-    scale: deviceScale,
-} = Dimensions.get('window');
+// declare the types for our dimension variables
+let deviceWidth: number;
+let deviceHeight: number;
+let deviceScale: number;
 
-export const wp = (percentage: number): number => Math.round(percentage * deviceWidth) / deviceScale;
-export const hp = (percentage: number): number => Math.round(percentage * deviceHeight) / deviceScale;
+// get the dimensions
+({ width: deviceWidth, height: deviceHeight, scale: deviceScale } = Dimensions.get('window'));
 
+/*
+ * @Function() wp
+ * @Description() This function returns the width of the device in percentage
+ * @Params() percentage
+ * @Return() number
+ *
+*/
+export const wp = (percentage: number): number => {
+    if (percentage < 0 || percentage > 100) {
+        // throw error
+        throw new Error('Percentage must be between 0 and 100');
+    }
+
+    // return
+    return Math.round((percentage / 100) * deviceWidth) / deviceScale;
+};
+
+/*
+ * @Function() hp
+ * @Description() This function returns the height of the device in percentage
+ * @Params() percentage
+ * @Return() number
+ *
+*/
+export const hp = (percentage: number): number => {
+    if (percentage < 0 || percentage > 100) {
+        // throw error
+        throw new Error('Percentage must be between 0 and 100');
+    }
+    
+    // return
+    return Math.round((percentage / 100) * deviceHeight) / deviceScale;
+};
