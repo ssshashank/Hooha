caac6c4912778c87b2e5a28d34469e738c8ec5b3
Shekhar S <shekharshashank97@gmail.com>
2024-10-13 15:00:52 +0530

    Constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,15 @@
+export const AUTH_ROUTES = {
+    LANDING: {
+        name: 'index',
+        path: '/',
+    },
+    LOGIN: {
+        name: 'login',
+        path: '/login',
+    },
+
+    //SIGNUP: 'signup',
+    //FORGOT_PASSWORD: 'forgot-password',
+    //RESET_PASSWORD: 'reset-password',
+};
+
