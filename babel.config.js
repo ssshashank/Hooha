4e8b0fd6cba2b5d63e5d592f83ff1976c0becf22
Shekhar S <shekharshashank97@gmail.com>
2024-10-26 20:34:54 +0530

    Config changes
    Config file changes

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
 module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            [
+                'module-resolver',
+                {
+                    alias: {
+                        '@': './src',
+                        '@Assets': './src/assets',
+                        '@Components': './src/components',
+                        '@Modules': './src/modules',
+                        '@Constants': './src/constants',
+                        '@Hooks': './src/hooks',
+                        '@Styles': './src/styles',
+                        '@Utils': './src/utils',
+                        '@Providers': './src/providers',
+                    },
+                },
+            ],
+        ],
+    };
 };
