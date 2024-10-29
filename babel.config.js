1b22658918395c422e550a47af587471a168701f
Shekhar S <shekharshashank97@gmail.com>
2024-10-29 16:42:14 +0530

    Config
    Babel config

--- babel.config.js
+++ babel.config.js
@@ -2,23 +2,23 @@ module.exports = function(api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
-        plugins: [
-            [
-                'module-resolver',
-                {
-                    alias: {
-                        '@': './src',
-                        '@Assets': './src/assets',
-                        '@Components': './src/components',
-                        '@Modules': './src/modules',
-                        '@Constants': './src/constants',
-                        '@Hooks': './src/hooks',
-                        '@Styles': './src/styles',
-                        '@Utils': './src/utils',
-                        '@Providers': './src/providers',
-                    },
-                },
-            ],
-        ],
+        //plugins: [
+        //    [
+        //        'module-resolver',
+        //        {
+        //            alias: {
+        //                '@': './src',
+        //                '@Assets': './src/assets',
+        //                '@Components': './src/components',
+        //                '@Modules': './src/modules',
+        //                '@Constants': './src/constants',
+        //                '@Hooks': './src/hooks',
+        //                '@Styles': './src/styles',
+        //                '@Utils': './src/utils',
+        //                '@Providers': './src/providers',
+        //            },
+        //        },
+        //    ],
+        //],
     };
 };
