b36769215b6e5016e6ce0efdb4261fc33cf3d82c
Shekhar S <shekharshashank97@gmail.com>
2025-01-29 21:36:09 +0530

    chore:
    Added fields in routes

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,10 +2,12 @@ export const AUTH_ROUTES = {
     LANDING: {
         name: 'index',
         path: '/',
+        type: '',
     },
     LOGIN: {
         name: 'login',
         path: '/login',
+        type: 'modal'
     },
 
     //SIGNUP: 'signup',
